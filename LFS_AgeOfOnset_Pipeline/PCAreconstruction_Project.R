2cf77db892adb283ca48fd565e4b502b27e0019a
vsubasri <vallisubasri@gmail.com>
2021-07-19 20:56:08 -0400

    remove parse options

--- LFS_AgeOfOnset_Pipeline/PCAreconstruction_Project.R
+++ LFS_AgeOfOnset_Pipeline/PCAreconstruction_Project.R
@@ -21,25 +21,18 @@ ind <- 44
 
 cat("[ Generate train/test/val split  ]","\n")
 data <- readRDS(paste0(outdir,"rds/",id,".rds"))
-data <- data[!is.na(data$ids),]
+beta <- data[ , grepl( "cg" , names( data ) ) ]
+clin <- data[ , -grepl( "cg" , names( data ) ) ]
 
 p_train <- readRDS('rds/batchEffectRemoval.rds')
 
 ## Predict projection onto PC space in test data
 cat("[ Predict PCA in Test Data ]","\n")
-test_pred <-predict(p_train,data_test[(ind+1):length(data_test)])
+test_pred <-predict(p_train,beta)
 Xhat_pred <- test_pred[, !(colnames(test_pred) %in% c(maxpc))] %*% t(p_train$rotation[, !(colnames(p_train$rotation) %in% c(maxpc))])
-beta_adj_pred <- scale(Xhat_pred, center = -(colMeans(data_test[(ind+1):length(data_test)])), scale = T)
-data_adj_test <- cbind(data_test[1:ind],beta_adj_pred)
+beta_adj_pred <- scale(Xhat_pred, center = -(colMeans(beta)), scale = T)
+data_adj_test <- cbind(clin,beta_adj_pred)
 saveRDS(data_adj_test,paste0(outdir, 'rds/',id,'_ProjPC2Adj_TestSet.rds'))
-
-## Predict projection onto PC space in validation data
-cat("[ Predict PCA in Validation Data ]","\n")
-val_pred <-predict(p_train,data_val[(ind+1):length(data_val)])
-Xhat_pred_val <- val_pred[, !(colnames(val_pred) %in% c(maxpc))] %*% t(p_train$rotation[, !(colnames(p_train$rotation) %in% c(maxpc))])
-beta_adj_pred_val <- scale(Xhat_pred_val, center = -(colMeans(data_val[(ind+1):length(data_val)])), scale = T)
-data_adj_val <- cbind(data_val[1:ind],beta_adj_pred_val)
-saveRDS(data_adj_val,paste0(outdir,'rds/',id,'_ProjPC2Adj_ValidationSet.rds'))
 
 ## Plot test set correction ##
 cat("[ Plotting PCA of corrected test data ]","\n")
@@ -55,16 +48,5 @@ pc_clin <- cbind(allCleaned[1:ind],pc$x)
 write.csv(pc_clin,paste0(outdir,'Output/',id,'_ProjPC2Adj_PCA.csv'),quote=F,row.names=F)
 generate_pcsummary(pc_clin,paste0(id,'_ProjPC2Adj_PCA_summary.csv'),outdir)
 generate_pcplots(pc_clin,paste0(id,'_ProjPC2Adj'),outdir)
-
-## Plot concordance between technical replicates ##
-duplicated_data <- get_technicalreplicates(allCleaned)
-plot_concordance(duplicated_data,paste0(id,"_ProjPC2Adj"),outdir)
-
-pc_beta <- data.frame(duplicated_data[(ind+1):length(duplicated_data)], row.names = paste0(duplicated_data$ids," (",duplicated_data$array,")"))
-pc <- prcomp(as.matrix(pc_beta), scale = TRUE)
-pc_clin <- cbind(duplicated_data[1:ind],pc$x)
-write.csv(pc_clin,paste0(outdir,'Output/',id,'_ProjPC2Adj_TechnicalReplicates_PCA.csv'),quote=F,row.names=F)
-generate_pcsummary(pc_clin,paste0(id,'_ProjPC2Adj_TechnicalReplicates_PCA_summary.csv'),outdir)
-generate_pcplots(pc_clin,paste0(id,'_ProjPC2Adj_TechnicalReplicates'),outdir)
 
 
--- LFS_AgeOfOnset_Pipeline/runSingleModel.R
+++ LFS_AgeOfOnset_Pipeline/runSingleModel.R
@@ -23,16 +23,6 @@ set.seed(123)
 parser <- ArgumentParser()
 parser$add_argument("-o","--outdir", action="store")
 parser$add_argument("-i","--id", action="store")
-parser$add_argument("-a","--aggregate", action="store")
-parser$add_argument("-r", "--array", action="store_true", default=FALSE, help="Include array as a covariate")
-parser$add_argument("-x", "--sex", action="store_true", default=FALSE, help="Include sex as a covariate")
-parser$add_argument("-c", "--cancer", action="store_true", default=FALSE, help="Remove cancer associated probes")
-parser$add_argument("-l", "--lfs", action="store_true", default=FALSE, help="Subset LFS probes")
-parser$add_argument("-g", "--gene", action="store_true", default=FALSE, help="Subset probes in gene")
-parser$add_argument("-s", "--scale", action="store_true", default=FALSE, help="Scale data")
-parser$add_argument("-d", "--canceratdraw", action="store_true", default=FALSE, help="Include cancer at draw variable")
-parser$add_argument("-t", "--systreat", action="store_true", default=FALSE, help="Include systemic treatment variable")
-args <- parser$parse_args()
 
 id <- args$id
 outdir <- args$outdir
@@ -52,86 +42,26 @@ data_test <- remove_age_probes(data_test)
 data_test <- remove_xRP(data_test)
 data_test <- remove_duplicates(data_test)
 
-if (args$lfs) {
-  cat(paste0("[ Get LFS probes ]","\n"))
-  data_train <- get_lfs_probes(data_train)
-  data_test <- get_lfs_probes(data_test)
-  id <- paste0(id,"lfs_")
-}
+cat(paste0("[ Get LFS probes ]","\n"))
+data_train <- get_lfs_probes(data_train)
+data_test <- get_lfs_probes(data_test)
+id <- paste0(id,"lfs_")
 
-if (args$gene) {
-  cat(paste0("[ Get probes in gene body ]","\n"))
-  data_train <- get_gene_probes(data_train)
-  data_test <- get_gene_probes(data_test)
-  id <- paste0(id,"gene_")
-}
-
-if (args$cancer) {
-  cancer_probes <- get_cancer_probes(data_train,id,outdir)
-  keep_probes <- colnames(data_train)[!colnames(data_train) %in% cancer_probes]
-  if (length(cancer_probes) != 0) {
-        cat(paste0("[ Remove cancer signal ]","\n"))
-        data_train <- data_train[keep_probes]
-        data_test <- data_test[keep_probes]
-        id <- paste0(id,"nocancer_")
-  } else {
-        cat(paste0("[ No significant cancer signal ]","\n"))
-        id <- paste0(id,"nocancerfailed_")
-  }
-}
-
-if (length(args$aggregate)!=0) {
-  cat(paste0("[ Aggregate by functional region ]","\n"))
-  data_train <- get_func_probes(data_train,args$aggregate)
-  data_test <- get_func_probes(data_test,args$aggregate)
-  id <- paste0(id,paste0(args$aggregate,"_"))
-}
+cat(paste0("[ Aggregate by functional region ]","\n"))
+data_train <- get_func_probes(data_train,args$aggregate)
+data_test <- get_func_probes(data_test,args$aggregate)
+id <- paste0(id,paste0(args$aggregate,"_"))
 
 
-if (args$scale) {
-  data_train <- scale_df(data_train)
-  data_test <- scale_df(data_test)
-  id <- paste0(id, "scaled_")
-}
+data_train <- scale_df(data_train)
+data_test <- scale_df(data_test)
+id <- paste0(id, "scaled_")
 
-if (args$canceratdraw) {
-  id <- paste0(id, "canceratdraw_")
-}
-
-if (args$systreat) {
-  id <- paste0(id, "systreat_")
-}
+id <- paste0(id, "canceratdraw_")
+id <- paste0(id, "systreat_")
 
 genes <- colnames(data_train)[45:length(data_train)]
 
 ## xgboost
-if (!file.exists(paste0(outdir,'rds/',id,'xgboost_ageofonset_results.rds'))) {
-  xgboost_results <- pred_cancer_xgboost(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes)
-  saveRDS(xgboost_results,paste0(outdir,'rds/',id,'xgboost_ageofonset_results.rds'))
-}
-## random forest
-if (!file.exists(paste0(outdir,'rds/',id,'rf_ageofonset_results.rds'))) {
-  rf_results <- pred_cancer_rf(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes)
-  saveRDS(rf_results,paste0(outdir,'rds/',id,'rf_ageofonset_results.rds'))
-}
-## enet
-if (!file.exists(paste0(outdir,'rds/',id,'enet_ageofonset_results.rds'))) {
-  enet_results <- pred_cancer_enet(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes)
-  saveRDS(enet_results,paste0(outdir,'rds/',id,'enet_ageofonset_results.rds'))
-}
-## svm linear
-if (!file.exists(paste0(outdir,'rds/',id,'svmLinear_ageofonset_results.rds'))) {
-  svmLinear_results <- pred_cancer_svm(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes,"svmLinear")
-  saveRDS(svmLinear_results,paste0(outdir,'rds/',id,'svmLinear_ageofonset_results.rds'))
-}
-## svm radial
-if (!file.exists(paste0(outdir,'rds/',id,'svmRadial_ageofonset_results.rds'))) {
-  svmRadial_results <- pred_cancer_svm(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes,"svmRadial")
-  saveRDS(svmRadial_results,paste0(outdir,'rds/',id,'svmRadial_ageofonset_results.rds'))
-}
-## gbm 
-if (!file.exists(paste0(outdir,'rds/',id,'gbm_ageofonset_results.rds'))) {
-  gbm_results <- pred_cancer_gbm(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes)
-  saveRDS(gbm_results,paste0(outdir,'rds/',id,'gbm_ageofonset_results.rds'))
-}
-
+xgboost_results <- pred_cancer_xgboost(data_train,data_test,72,args$sex,args$array,args$canceratdraw,args$systreat,genes)
+saveRDS(xgboost_results,paste0(outdir,'rds/',id,'xgboost_ageofonset_results.rds'))
