248c76dc5695eb3d629f3babd1b88e2f30bcee8c
blaverty <blaverty@hotmail.ca>
2023-02-23 13:17:00 -0500

    initialize IDOL directory

---
+++ LFS_AgeOfOnset_IDOL/run_rgset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -t 200:00:00
+#SBATCH -N 1 -c 40
+#SBATCH --mem=50g
+#SBATCH -o /hpf/largeprojects/davidm/blaverty/ageofonset/logs
+#SBATCH -e /hpf/largeprojects/davidm/blaverty/ageofonset/logs
+
+
+
+module load R/4.0.0
+
+Rscript rgset.R
---
+++ LFS_AgeOfOnset_IDOL/run_umap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="umap"
+#SBATCH -t 10:00:00
+#SBATCH -N 1 -c 30
+#SBATCH --mem 50g
+#SBATCH -o /hpf/largeprojects/davidm/blaverty/ageofonset/logs/umap_o
+#SBATCH -e /hpf/largeprojects/davidm/blaverty/ageofonset/logs/umap_e
+
+
+
+module load R/4.0.0
+
+Rscript umap.R 
